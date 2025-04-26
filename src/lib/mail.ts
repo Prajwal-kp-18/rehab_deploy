6e276d0c15a170c548e0e403c6e2f9a5d1ebfdd8
Prajwal-kp-18 <prajwal.kp.1817@gmail.com>
2025-04-26 11:54:04 +0530

    fixed mailing issues

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `${process.env.HOST_URL}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "mail@khetideals.shop",
@@ -13,7 +13,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   });
 };
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `${process.env.HOST_URL}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "mail@khetideals.shop",
